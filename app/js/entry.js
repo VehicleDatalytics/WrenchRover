b1fbb9a368b8fe5cd949a08ed341cae8c4123dcf
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-07-20 17:02:00 -0700

    fixing static destination, scaffolding entry.

---
+++ app/js/entry.js
@@ -0,0 +1,17 @@
+// const angular = require('angular');
+// const wrapp = angular.module('wrapp', [require('angular-route')]);
+//
+//
+// wrapp.config(['$routeProvider', function($rp) {
+//   $rp
+//     .when('/', {
+//     //   templateUrl: 'templates/ ...  .html'
+//     //   controller: 'xController',
+//     //   controllerAs: 'xctrl'
+//     })
+//     .otherwise({
+//       redirectTo: '/'
+//     });
+// }]);
+
+console.log('nitro cold brew');
