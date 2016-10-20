bf53daf8f837f21bef615890c3152bb6e2542e29
Shelly Yusuf <ShellyNYusuf@gmail.com>
2016-10-20 13:39:54 -0700

    scaffolding service center portal.

--- app/js/entry.js
+++ app/js/entry.js
@@ -39,6 +39,7 @@ require('./modal_button')(wrApp);
 require('./nav')(wrApp);
 require('./modal_button_sign_in')(wrApp);
 require('./modal_window_sign_in')(wrApp);
+require('./sc_portal')(wrApp);
 
 
 wrApp.config(function($stateProvider, $urlRouterProvider) {
@@ -110,6 +111,12 @@ wrApp.config(function($stateProvider, $urlRouterProvider) {
   templateUrl: 'templates/user/views/user_dashboard.html'
   // controller:
   // controllerAs:
+})
+.state('sc_portal_view', {
+  url: '/scportal',
+  templateUrl: 'templates/sc_portal/views/sc_portal_view.html',
+  controller: 'scPortalController',
+  controllerAs: 'portal'
 })
 
 
