4e1a75f917b094557679fa69e4a4c3ca7e14c693
Shelly Yusuf <ShellyNYusuf@gmail.com>
2016-11-13 14:46:03 -0800

    adding date/ time availability option. Currently using html5 date/time input options. Only showing one date right now. Will add the other two, later

--- app/js/services/sc_comm_service.js
+++ app/js/services/sc_comm_service.js
@@ -10,17 +10,19 @@
         return {
           createQuote: function(x, y, z, a) {
             this.quoteObj = x;
-            console.log('just weeping');
             console.log(x, y);
 
 
             this.quote_X = y;
             this.quote_X.user_id = x;
+            // console.log(this.quote_X.availible_X.toTimeString());
+            console.log(this.quote_X.time_one.toLocaleTimeString());
+
+            this.quote_X.availible_dates = this.quote_X.date_one.toLocaleDateString() + ' at ' + this.quote_X.time_one.toLocaleTimeString();
+            console.log(this.quote_X.availible_dates);
 
             this.quote_X.service_center_id = JSON.parse(localStorage.getItem('service_center_id'));
             this.quote_X.service_request_id = z;
-            // this.quote_X.quote_text = a;
-            // this.quote_X.service_center_id = 17;
             console.log(this.quote_X);
             $http.post(baseUrl + 'service_quotes', this.quote_X).
             success((config) => {
