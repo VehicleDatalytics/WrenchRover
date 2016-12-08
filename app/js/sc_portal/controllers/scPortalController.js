2ff021a4d359445758b8c271171401b564397a6b
Shelly Yusuf <ShellyNYusuf@gmail.com>
2016-12-07 17:03:43 -0800

    adding files

--- app/js/sc_portal/controllers/scPortalController.js
+++ app/js/sc_portal/controllers/scPortalController.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
       console.log('iniTting the functions!!!!!');
     };
 
+
     this.mytime = new Date();
-    console.log(this.mytime);
     this.ismeridian = true;
 
     this.timeChange = function(value, value2, value3) {
@@ -18,13 +18,20 @@ module.exports = function(app) {
     };
 
     this.service = scCommService;
-
     this.servicerequests = [];
     this.workrequests = [];
     this.servicequotes = [];
     this.pastbids = [];
     this.acceptedbids = [];
 
+    this.appointmentTimes = ['8:00 a.m', '8:30 a.m', '9:00 a.m', '9:30 a.m', '10:00 a.m', '10:30 a.m', '11:00 a.m', '11:30 a.m', '12:00 p.m', '12:30 p.m', '1:00 p.m', '1:30 p.m', '2:00 p.m', '2:30 p.m', '3:00 p.m', '3:30 p.m', '4:00 p.m', '4:30 p.m', '5:00 p.m', '5:30 p.m', '6:00 p.m', '6:30 p.m', '7:00 p.m', '7:30 p.m.', '8:00 p.m.'];
+
+
+    this.pickATime = function(time) {
+      console.log(time);
+
+      return time;
+    };
 
     this.getAll = () => {
       $http.get(baseUrl + '/service_requests')
@@ -49,22 +56,24 @@ module.exports = function(app) {
       });
     };
 
+
     var _this = this;
     this.activeDate = null;
 
-    // this.selectedDates = [new Date().setHours(0, 0, 0, 0)];
     this.selectedDates = [];
+
     if (this.activeDate = null) {
-    //   this.selectedDates.push(new Date().setHours(0, 0, 0, 0));
+//   this.selectedDates.push(new Date().setHours(0, 0, 0, 0));
       this.selectedDates.push(new Date().toLocaleDateString);
-    //   this.selectedDates.push('whatever');
+//   this.selectedDates.push('whatever');
       console.log('stuff');
       console.log(this.selectedDates);
     }
+
     this.type = 'individual';
     this.options = {
-    //   startingDay: 1,
-    //   minDate: new Date(),
+//   startingDay: 1,
+//   minDate: new Date(),
       customClass: function(data) {
         if (_this.selectedDates.indexOf(data.date) > -1 && _this.selectedDates.length <= 3) {
           return 'selected';
@@ -72,6 +81,9 @@ module.exports = function(app) {
         return '';
       }
     };
+
+
+    this.type = 'individual';
 
     this.show2pickers = false;
 
@@ -88,26 +100,6 @@ module.exports = function(app) {
       scCommService.addDates(value);
     };
 
-    // this.quoteObj = {
-    //   user_id: null,
-    //   service_center_id: null,
-    //   service_request_id: 169,
-    //   quote_text: null,
-    //   quote_cost: null
-    // };
-
-    // this.addDates = function(value) {
-    //   console.log('adding dates');
-    //   for (var i = 0; i < value.length; i++) {
-    //     console.log(new Date(value[i]));
-    //   }
-    // };
-
-        //   var h = d.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' }) .replace(/(:\d{2}| [AP]M)$/, '');
-
-    // h.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' });
-    // i.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' });
-    // j.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' });
 
     this.createQuote = function(x, y, z, d, e, f) {
       console.log('creating the quote');
@@ -159,20 +151,9 @@ module.exports = function(app) {
             }
 
           }
-
-
-          // //
-        //   this.servicequotes.splice(0);
-        //   for (var i = 0; i < res.data.length; i++) {
-        //     this.servicequotes.push(res.data[i]);
-        //   }
-
-          // //
-
-        //   console.log(this.service_request_id);
         }
       });
-    // this.workrequests.splice(0);
+
 
     };
   }
